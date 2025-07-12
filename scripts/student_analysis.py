0bb512308a9d83cac02f74c94ad8e2f76badccc3
dehaarte <jahdehaarte@gmail.com>
2025-07-12 04:52:26 -0400

    To visualize relationships between variables, implemented an histogram plot showing the distribution of CGPA by depression Status in student_analysis.ipynb file, and implemented the same histogram plot as a function in the scripts/student_analysis.py. Created the baseline_model.ipynb file added some imports, loaded the dataset, and binary encoded the Yes/No values columns "Have you ever had suicidal thoughts ?", and” Family History of Mental Illness”. Created a column_value_counts.ipynb file to see the all the values of City, Profession, Sleep Duration, Dietary Habits, and Degree properly.

--- scripts/student_analysis.py
+++ scripts/student_analysis.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
-def load_data(path="../data/student_depression_dataset.csv"):
+def load_data(path=".../data/student_depression_dataset.csv"):
     return pd.read_csv(path)
 
 def target_distribution(df, target_column="Depression"):
@@ -13,6 +13,16 @@ def target_distribution(df, target_column="Depression"):
     plt.title("Depression Distribution")
     plt.show()
 
+# When you close the target distribution window, the plot below will appear.
+def cgpa_distribution(df, bins=20):
+    sns.histplot(data=df, x="CGPA", hue="Depression", bins=bins, multiple="dodge", hue_order=[0,1])
+    plt.xticks(range(0,11))
+    plt.title("CGPA Distribution by Depression Status")
+    plt.xlabel("CGPA")
+    plt.ylabel("Number of Students")
+    plt.show()
+
 if __name__ == "__main__":
     df = load_data()
     target_distribution(df)
+    cgpa_distribution(df, bins=20)
