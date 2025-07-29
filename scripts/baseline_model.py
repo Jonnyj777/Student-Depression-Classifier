ec3319fae4ae2789098ef94c5c257970f18704c4
LemonadeCodeGirl <42980263+LemonadeCodeGirl@users.noreply.github.com>
2025-07-28 20:26:38 -0400

    Added Random forrest to the baseline model

--- scripts/baseline_model.py
+++ scripts/baseline_model.py
@@ -4,13 +4,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_text
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
 
 from evaluation import plot_cf, get_f1_score, get_precision_score
 
 def load_data():
-    return pd.read_csv("../data/student_depression_dataset.csv")
+    return pd.read_csv("data/student_depression_dataset.csv")
 
 def train_baseline_model():
     df = load_data()
@@ -101,10 +104,81 @@ def train_baseline_model():
 
     return y_test, y_pred_best
 
+def random_forest():
+    print("Starting Random Forest")
+
+    df = pd.read_csv("data/student_depression_dataset.csv", encoding="latin-1")
+
+    # Encode categorical variables
+    df = pd.get_dummies(df)
+
+    # Drop rows with missing values (optional but often necessary)
+    df = df.dropna()
+
+    X = df.drop(columns=['Depression'])
+    y = df['Depression']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
+    
+    rf = RandomForestClassifier()
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+
+    importances = rf.feature_importances_
+    feature_names = X_train.columns
+
+    importance_df = pd.DataFrame({
+        'Feature': feature_names,
+        'Importance': importances
+    })
+
+    # Sort by importance (descending)
+    importance_df = importance_df.sort_values(by='Importance', ascending=False)
+    print(importance_df)
+
+    # print(y_pred)
+
+    # print("Accuracy of Random Forest 1: " + str(rf.score(X_test, y_test)))
+    print("Accuracy of Random Forest 1 (all features): " + str(accuracy_score(y_test, y_pred)))
+    print("F1 Score of Random Forest 1 (all features): " + str(f1_score(y_test, y_pred)))
+
+    #########################################################
+    
+    search = RandomizedSearchCV(
+        RandomForestClassifier(random_state=42),
+        {
+            'n_estimators': [100, 200],
+            'max_depth': [10, 20, None],
+            'max_features': ['sqrt'],
+            'min_samples_split': [2, 5],
+            'min_samples_leaf': [1, 2]
+        },
+        n_iter=10,
+        scoring='f1_weighted',
+        cv=3,
+        n_jobs=-1
+    )
+    print(search.get_params())
+    search.fit(X_train, y_train)
+    model = search.best_estimator_
+
+    y_pred_best = model.predict(X_test)
+
+    print("Accuracy of Random Forest 2 (best model): " + str(accuracy_score(y_test, y_pred_best)))
+    print("F1 Score of Random Forest 2 (best model): " + str(f1_score(y_test, y_pred_best)))
+
+    return y_test, y_pred_best
+
+
 if __name__ == "__main__":
     y_test, y_pred = train_baseline_model()
 
     labels = ['Not Depressed', 'Depressed']
+    plot_cf(y_test, y_pred, labels)
+
+    print("Random Forest")
+    y_test, y_pred = random_forest()
     plot_cf(y_test, y_pred, labels)
 
 
@@ -116,3 +190,5 @@ if __name__ == "__main__":
 # - https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.RandomizedSearchCV.html
 # - https://www.geeksforgeeks.org/machine-learning/comparing-randomized-search-and-grid-search-for-hyperparameter-estimation-in-scikit-learn/
 # - https://www.analyticsvidhya.com/blog/2022/11/hyperparameter-tuning-using-randomized-search/
+# - https://www.geeksforgeeks.org/machine-learning/random-forest-algorithm-in-machine-learning/
+# - https://www.youtube.com/watch?v=_QuGM_FW9eo
--- scripts/randomForest.py
+++ scripts/randomForest.py
@@ -2,15 +2,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import RandomizedSearchCV
 
-def main():
+
+# from sklearn.metrics import classification_report, confusion_matrix
+
+def random_forest():
     print("Starting Random Forest")
 
     df = pd.read_csv("data/student_depression_dataset.csv", encoding="latin-1")
-
-
-    df = df.drop(columns=['Gender'])
 
     # Encode categorical variables
     df = pd.get_dummies(df)
@@ -18,15 +19,9 @@ def main():
     # Drop rows with missing values (optional but often necessary)
     df = df.dropna()
 
-    # print the shape of the dataframe
-    print(df.shape)
+    X = df.drop(columns=['Depression'])
+    y = df['Depression']
 
-    # print the columns of the dataframe
-    print(df.columns)
-
-    X = df.iloc[:, 0:13] #???
-    y = df.iloc[:, 13]
-    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
     
     rf = RandomForestClassifier()
@@ -34,29 +29,49 @@ def main():
 
     y_pred = rf.predict(X_test)
 
-    print(y_pred)
+    importances = rf.feature_importances_
+    feature_names = X_train.columns
 
-    print(rf.score(X_test, y_test))
+    importance_df = pd.DataFrame({
+        'Feature': feature_names,
+        'Importance': importances
+    })
 
-    print(classification_report(y_test, y_pred))
+    # Sort by importance (descending)
+    importance_df = importance_df.sort_values(by='Importance', ascending=False)
+    print(importance_df)
 
-    features = pd.DataFrame(rf.feature_importances_, index=X.columns)
+    # print(y_pred)
 
-    print(features) # Take a look at the features and their importance
+    # print("Accuracy of Random Forest 1: " + str(rf.score(X_test, y_test)))
+    print("Accuracy of Random Forest 1 (all features): " + str(accuracy_score(y_test, y_pred)))
+    print("F1 Score of Random Forest 1 (all features): " + str(f1_score(y_test, y_pred)))
 
-    rf2 = RandomForestClassifier(n_estimators=1000, 
-                                criterion='entropy',
-                                max_depth=14,
-                                min_samples_split=10,
-                                random_state=17)
+    #########################################################
+    
+    search = RandomizedSearchCV(
+        RandomForestClassifier(random_state=42),
+        {
+            'n_estimators': [100, 200],
+            'max_depth': [10, 20, None],
+            'max_features': ['sqrt'],
+            'min_samples_split': [2, 5],
+            'min_samples_leaf': [1, 2]
+        },
+        n_iter=10,
+        scoring='f1_weighted',
+        cv=3,
+        n_jobs=-1
+    )
+    print(search.get_params())
+    search.fit(X_train, y_train)
+    model = search.best_estimator_
 
-    rf2.fit(X_train, y_train)
+    y_pred_best = model.predict(X_test)
 
-    print(rf2.score(X_test, y_test))
-
-    y_pred2 = rf2.predict(X_test)   
-    print(classification_report(y_test, y_pred2))
+    print("Accuracy of Random Forest 2 (best model): " + str(accuracy_score(y_test, y_pred_best)))
+    print("F1 Score of Random Forest 2 (best model): " + str(f1_score(y_test, y_pred_best)))
 
 
 if __name__ == "__main__":
-    main()
+    random_forest()
